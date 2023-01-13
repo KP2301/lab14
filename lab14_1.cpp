ca6f29fec223d6c6ebd2e96fd3d5f611538f3140
Kittiwin <kittiwinktb@gmail.com>
2023-01-13 16:10:29 +0700

    lab14_1complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[] , int n  , double b[]){
+    double sum1 = 0, sum2 = 0, sum4 = 0, max = a[0], min = a[0], sum3 = 1;
+    for(int i = 0; i < n ;i++){
+        sum1 += a[i];
+        sum3 *= a[i];
+        sum4 += (1/a[i]);
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+    }
+    b[0] = sum1/n;
+    for(int i = 0; i < n ; i++){
+        sum2 += pow(a[i] - b[0],2) ;
+    }
+    b[1] = sqrt(sum2/n);
+    b[2] = pow(sum3,(1.0/n));
+    b[3] = n/sum4;
+    b[4] = max;
+    b[5] = min;
+}
