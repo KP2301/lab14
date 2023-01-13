e573816ee30c680fa361cd08b4386ca0cff8b477
Kittiwin <kittiwinktb@gmail.com>
2023-01-13 16:12:33 +0700

    lab14_2complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool I[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M ;j++){
+            
+            if(I[i][j] == 1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+            
+        }
+        cout << "|" << endl; 
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
+
+void updateImage(bool I[][M],int S,int X,int Y){
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < M; j++){
+            if(sqrt(pow((i - X),2) + pow((j - Y),2)) <= (S-1)){
+                I[i][j] = 1;
+            }
+        }
+    }
+   
+}
